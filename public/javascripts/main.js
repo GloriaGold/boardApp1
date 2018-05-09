28f66a0c0af9987871bc5936383db3c0b6b63008
Gloria Schulz <gloria.johnson.a@gmail.com>
2018-05-09 15:46:17 +0200

    Add main js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,2 +1,14 @@
 import Header from './Header.js'
 const header = new Header('[data-module-header]')
+
+import { $, $$ } from './utils/helpers.js'
+
+const button = $('.js-button')
+const frontpage = $('#frontpage')
+const newBoard = $('#newBoard')
+
+button.addEventListener('click', event => {
+  frontpage.classList.toggle('hidden')
+  newBoard.classList.toggle('hidden')
+  // gezeigt werden soll die seite mit der Id newBoard und der jeweilige inhalt der in der div ist. innerHTML
+})
