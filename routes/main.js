6bfc2dc21f404ed79dc5bb03b3c4ae44c1153366
Gloria Schulz <gloria.johnson.a@gmail.com>
2018-05-09 14:33:52 +0200

    Set up git

---
+++ routes/main.js
@@ -0,0 +1,12 @@
+import { $, $$ } from '.utils/helpers.js'
+
+const button = $('.js-button')
+const frontpage = $('#frontpage')
+const newBoard = $('#newBoard')
+
+button.addEventListener('click', event => { 
+    return 
+    // gezeigt werden soll die seite mit der Id newBoard und der jeweilige inhalt der in der div ist. innerHTML
+
+
+})
