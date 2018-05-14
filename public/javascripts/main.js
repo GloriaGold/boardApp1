a94b9b0d6326f9df23c9b0aa6db7e1e62cae9620
Gloria Schulz <gloria.johnson.a@gmail.com>
2018-05-14 11:20:04 +0200

    Start page 3. new board and edit the JS

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,3 +12,44 @@ button.addEventListener('click', event => {
   newBoard.classList.toggle('hidden')
   // gezeigt werden soll die seite mit der Id newBoard und der jeweilige inhalt der in der div ist. innerHTML
 })
+
+const formEl = document.querySelector('.task-form')
+const inputEl = document.querySelector('.task-form__input')
+const buttonEl = document.querySelector('.task-form__btn')
+const placeholderEl = document.querySelector('.todo__placeholder')
+const templateEl = document.querySelector('.template')
+const templateString = templateEl.innerHTML
+let todos = []
+
+formEl.addEventListener('submit', addTodo)
+
+function addTodo(event) {
+  event.preventDefault()
+  todos = [...todos, inputEl.value]
+  placeholderEl.innerHTLML = ''
+  todos.forEach(insertListItem)
+
+  inputEl.value = ''
+  inputEl.focus()
+}
+
+function insertListItem(todoText) {
+  const listItem = templateString.replace('#value#', todoText)
+  placeholderEl.insertAdjacentHTML('beforebegin', listItem)
+}
+
+function onSubmit(event) {
+  event.preventDefault()
+  const listItem = templateString.replace('#value#', inputEl.value)
+  placeholderEl.insertAdjacentHTML('beforebegin', listItem)
+  inputEl.value = ''
+  inputEl.focus()
+}
+
+/*const button = $('[data-button]')
+const frontpage = $('[data-overview]')
+const newBoard = $('#newBoard')
+
+button.addEventListener('click', event => {
+  frontpage.classList.toggle('hidden')
+  newBoard.classList.toggle('hidden') */
